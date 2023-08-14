9d2a6522f1660733baa148a8b46b0f585b2e2b3a
weizhu365 <zhuw10@nih.gov>
2023-08-14 13:57:49 -0400

    minor change

--- run_it_cgr.sh
+++ run_it_cgr.sh
@@ -5,6 +5,7 @@
 
 
 # conda activate snakemake
+mkdir -p /data/DCEG_Trios/new_cgr_data/TriosCompass_v2/TMP
 export TMPDIR=/data/DCEG_Trios/new_cgr_data/TriosCompass_v2/TMP
 
 snakemake --skip-script-cleanup -k  --keep-incomplete --rerun-incomplete --profile workflow/profiles/biowulf --verbose -p --use-conda --jobs 400 --use-envmodules --latency-wait 600 -T 0 -s Snakefile
