f1f7d55d46b849ac335c8f91cc5775cd08b1cb62
weizhu365 <zhuw10@nih.gov>
2024-03-19 11:21:27 -0400

    update workflow

--- run_it_chernobyl.sh
+++ run_it_chernobyl.sh
@@ -6,8 +6,12 @@
 
 # conda activate snakemake
 # export TMPDIR=/data/DCEG_Chernobyl/NP0436-HE5/GIAB_DATA/TMP
+mkdir -p TMP
+export TMPDIR=TMP
+
+module load singularity 
 
 # -k to keep going even on errors
-snakemake --verbose --skip-script-cleanup -k  --keep-incomplete --rerun-incomplete --profile workflow/profiles/biowulf --verbose -p --use-conda --jobs 800 --use-envmodules --latency-wait 120 -T 0  -s Snakefile_chernobyl
+snakemake --verbose --skip-script-cleanup -k  --keep-incomplete --ignore-incomplete --profile workflow/profiles/biowulf --verbose -p --use-conda --jobs 800 --use-singularity --use-envmodules --latency-wait 120 -T 0  -s Snakefile_chernobyl
 
  
