36018280d719a248388c411bf90903b713609cd4
Wei Zhu <weizhu365@gmail.com>
2025-07-31 09:06:22 -0400

    add config and others for GIAB runs

---
+++ data/launch_40X.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=200:00:00
+#SBATCH -o ${PWD}/snakemake.%j.out
+#SBATCH -e ${PWD}/snakemake.%j.err
+
+### or make sure singularity is available in alternative way
+module load singularity
+
+mkdir -p TMP
+export TMPDIR=TMP
+
+snakemake --profile TriosCompass_v2/workflow/profiles/slurm --configfile GIAB_40X.yaml \
+          --conda-frontend mamba 
+        #   \
+        #   --groups vizaln=group0 extract_parental_origin=group1 \
+        #   --group-components group0=20 group1=20 
